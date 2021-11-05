0ea8441fd505ee71dfc42e21d331a3f19e56e5cc
haanhtuanph09959 <tuanha@classfunc.com>
2021-11-05 16:21:57 +0700

    developer

---
+++ src/components/ModalStyle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Modal from '@react-na';
+const ModalStyle = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ModalStyle;
+
+const styles = StyleSheet.create({});
