889bfcc05ba934145c1a3daf1270526fecbf1eb3
linhpb <linhpb@react-plus.com>
2021-11-26 21:27:37 +0700

    add job (26/11)

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import ForgotPassword from './ForgotPassword';
 import LogIn from './LogIn';
+import POSTJOB from './AddJob';
 
 const rootReducer = combineReducers({
   Authen: LogIn,
   Forgot: ForgotPassword,
+  POSTJOB: POSTJOB,
 });
 
 export default (state, action) => rootReducer(state, action);
--- src/screen/NTD/TD/DangTin.js
+++ src/screen/NTD/TD/DangTin.js
@@ -12,10 +12,19 @@ import {scale} from 'react-native-size-matters';
 import {BackIcon, Selecter, DateIcon} from '../../../../assets/icon';
 import CircleCheckBox, {LABEL_POSITION} from 'react-native-circle-checkbox';
 import Sagaly from './Sagaly';
+import {useDispatch, useSelector} from 'react-redux';
+import {addPostJob} from '../../../redux/actions/actions';
+import {validateEmail, isVietnamesePhoneNumber} from '../../../base/Validate';
+import ModalStyle from '../../../components/ModalStyle';
 
 const DangTin = ({navigation}) => {
+  const dispatch = useDispatch();
+  const createdBy = useSelector(state => state.Authen.data.user.userId);
   const [check, setCheck] = useState(false);
-
+  console.log(
+    'Data: ',
+    useSelector(state => state),
+  );
   const b = {id: 2, ca: 'ca2'};
   const [DATA, setDaTa] = useState([
     {
@@ -23,30 +32,30 @@ const DangTin = ({navigation}) => {
       ca: 'ca1',
     },
   ]);
+  const [job_posting_position, set_job_posting_position] = useState('');
+  const [career, set_career] = useState('');
+  const [quantity_recruited, set_quantity_recruited] = useState('');
+  const [work_location, set_work_location] = useState(null);
+  const [working_form, set_working_form] = useState('');
+  const [salary, set_salary] = useState('');
+  const [min_education, set_min_education] = useState(null);
+  const [probation, set_probation] = useState(null);
+  const [rose, set_rose] = useState(null);
+  const [experience, set_experience] = useState(null);
+  const [posting_date, set_posting_date] = useState('');
+  const [last_date, set_last_date] = useState(null);
+  const [shift, set_shift] = useState(null);
+  const [start_time, set_start_time] = useState('');
+  const [end_time, set_end_time] = useState(null);
+  const [job_description, set_job_description] = useState('');
+  const [job_requirements, set_job_requirements] = useState('');
+  const [benefits_enjoyed, set_benefits_enjoyed] = useState('');
+  const [records_include, set_records_include] = useState('');
+  const [contact_person, set_contact_person] = useState('');
+  const [contact_address, set_contact_address] = useState('');
+  const [contact_phone, set_contact_phone] = useState('');
+  const [contact_email, set_contact_email] = useState('');
 
-  const [title, setTitle] = useState('');
-  const [soluong, setSoluong] = useState('');
-  const [cate, setCate] = useState(null);
-  const [capbac, setCapbac] = useState(null);
-  const [thuviec, setThuviec] = useState('');
-  const [hoahong, setHoahong] = useState('');
-  const [city, setCity] = useState(null);
-  const [qh, setQh] = useState(null);
-  const [htlv, setHtlv] = useState(null);
-  const [ht_luong, setHt_luong] = useState(null);
-  const [mucluong, setMucluong] = useState('');
-  const [traluong, setTraluong] = useState(null);
-  const [hocvan, setHocvan] = useState(null);
-  const [mota, setMota] = useState('');
-  const [gioitinh, setGioitinh] = useState(null);
-  const [yeucau, setYeucau] = useState('');
-  const [quyenloi, setQuyenloi] = useState('');
-  const [hoso, setHoso] = useState('');
-  const [namelh, setNamelh] = useState('');
-  const [phonelh, setPhonelh] = useState('');
-  const [addlh, setAddlh] = useState('');
-  const [emaillh, setEmaillh] = useState('');
-  const [timeFirst, setTimefirst] = useState('');
   const [timelast, setTimelast] = useState('');
   const [ca1timefirst, setCa1timefirst] = useState('');
   const [ca1timelast, setCa1timelast] = useState('');
@@ -67,6 +76,70 @@ const DangTin = ({navigation}) => {
   const [ca5timefirst, setCa5timefirst] = useState('');
   const [ca5timelast, setCa5timelast] = useState('');
   const [day5, setday5] = useState('');
+
+  const [modal, setModal] = useState(false);
+  const [error, setError] = useState('');
+
+  const onSubmit = () => {
+    if (
+      !job_posting_position ||
+      !career ||
+      !quantity_recruited ||
+      !work_location ||
+      !working_form ||
+      !salary ||
+      !min_education ||
+      !probation ||
+      !rose ||
+      !job_requirements ||
+      !benefits_enjoyed ||
+      !records_include ||
+      !contact_person ||
+      !contact_address ||
+      !contact_phone ||
+      !contact_email
+    ) {
+      setModal(true);
+      setError('Các ô nhập là bắt buộc không được để trống! ');
+    } else if (!validateEmail(contact_email)) {
+      setModal(true);
+      setError('Bạn nhập email không đúng định dạng. Vui lòng nhập lại ! ');
+    } else if (!isVietnamesePhoneNumber(contact_phone)) {
+      setModal(true);
+      setError(
+        'Bạn nhập số điện thoại không đúng định dạng. Vui lòng nhập lại ! ',
+      );
+    } else {
+      dispatch(
+        addPostJob(
+          job_posting_position,
+          career,
+          quantity_recruited,
+          work_location,
+          working_form,
+          salary,
+          min_education,
+          probation,
+          rose,
+          experience,
+          posting_date,
+          last_date,
+          shift,
+          start_time,
+          end_time,
+          job_description,
+          job_requirements,
+          benefits_enjoyed,
+          records_include,
+          contact_person,
+          contact_address,
+          contact_phone,
+          contact_email,
+          createdBy,
+        ),
+      );
+    }
+  };
   const renderItem = ({}) => (
     <View style={styles.ViewFL}>
       <Text style={styles.TextTitle}>Thời gian</Text>
@@ -125,8 +198,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: Đầu bếp"
                 style={styles.textInput}
-                value={title}
-                onChangeText={input => setTitle(input)}
+                value={job_posting_position}
+                onChangeText={input => set_job_posting_position(input)}
               />
             </View>
           </View>
@@ -134,7 +207,12 @@ const DangTin = ({navigation}) => {
           <View>
             <Text style={styles.TextTitle}>Ngành nghề *</Text>
             <View style={[styles.boxInput, {flexDirection: 'row'}]}>
-              <TextInput placeholder="VD: Đầu bếp" style={styles.textInput} />
+              <TextInput
+                placeholder="VD: Đầu bếp"
+                style={styles.textInput}
+                value={career}
+                onChangeText={input => set_career(input)}
+              />
               <TouchableOpacity
                 style={styles.Selecter}
                 onPress={() => navigation.navigate('Job')}>
@@ -146,14 +224,24 @@ const DangTin = ({navigation}) => {
           <View>
             <Text style={styles.TextTitle}>Số lượng cần tuyển *</Text>
             <View style={styles.boxInput}>
-              <TextInput placeholder="VD: 10" style={styles.textInput} />
+              <TextInput
+                placeholder="VD: 10"
+                style={styles.textInput}
+                value={quantity_recruited}
+                onChangeText={text => set_quantity_recruited(text)}
+              />
             </View>
           </View>
           {/* selcter */}
           <View>
             <Text style={styles.TextTitle}>Cấp bậc *</Text>
             <View style={[styles.boxInput, {flexDirection: 'row'}]}>
-              <TextInput placeholder="Chọn cấp bậc" style={styles.textInput} />
+              <TextInput
+                placeholder="Chọn cấp bậc"
+                style={styles.textInput}
+                value={min_education}
+                onChangeText={text => set_min_education(text)}
+              />
               <TouchableOpacity style={styles.Selecter}>
                 <Selecter />
               </TouchableOpacity>
@@ -164,7 +252,12 @@ const DangTin = ({navigation}) => {
           <View>
             <Text style={styles.TextTitle}>Địa điểm làm việc *</Text>
             <View style={[styles.boxInput, {flexDirection: 'row'}]}>
-              <TextInput placeholder="Chọn địa điểm" style={styles.textInput} />
+              <TextInput
+                placeholder="Chọn địa điểm"
+                style={styles.textInput}
+                value={work_location}
+                onChangeText={text => set_work_location(text)}
+              />
               <TouchableOpacity style={styles.Selecter}>
                 <Selecter />
               </TouchableOpacity>
@@ -192,6 +285,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="Chọn hình thức"
                 style={styles.textInput}
+                value={working_form}
+                onChangeText={text => set_working_form(text)}
               />
               <TouchableOpacity style={styles.Selecter}>
                 <Selecter />
@@ -206,6 +301,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="Chọn mức lương"
                 style={styles.textInput}
+                value={salary}
+                onChangeText={text => set_salary(text)}
               />
               <TouchableOpacity
                 style={styles.Selecter}
@@ -232,7 +329,12 @@ const DangTin = ({navigation}) => {
           <View>
             <Text style={styles.TextTitle}>Thời gian thử việc *</Text>
             <View style={styles.boxInput}>
-              <TextInput placeholder="VD: 1 tuần" style={styles.textInput} />
+              <TextInput
+                placeholder="VD: 1 tuần"
+                style={styles.textInput}
+                value={probation}
+                onChangeText={text => set_probation(text)}
+              />
             </View>
           </View>
           <View>
@@ -241,6 +343,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: Từ 10%-20%"
                 style={styles.textInput}
+                value={rose}
+                onChangeText={text => set_rose(text)}
               />
             </View>
           </View>
@@ -327,6 +431,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="Nhập tối thiểu 50 từ"
                 style={styles.textInput}
+                value={job_requirements}
+                onChangeText={text => set_job_requirements(text)}
               />
             </View>
           </View>
@@ -339,6 +445,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="Nhập tối thiểu 50 từ"
                 style={styles.textInput}
+                value={benefits_enjoyed}
+                onChangeText={text => set_benefits_enjoyed(text)}
               />
             </View>
           </View>
@@ -351,6 +459,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="Nhập tối thiểu 50 từ"
                 style={styles.textInput}
+                value={records_include}
+                onChangeText={text => set_records_include(text)}
               />
             </View>
           </View>
@@ -363,6 +473,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: Hà Anh Tuấn"
                 style={styles.textInput}
+                value={contact_person}
+                onChangeText={text => set_contact_person(text)}
               />
             </View>
           </View>
@@ -372,6 +484,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: 05 Cẩm Khê"
                 style={styles.textInput}
+                value={contact_address}
+                onChangeText={text => set_contact_address(text)}
               />
             </View>
           </View>
@@ -381,6 +495,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: Từ 0367289911"
                 style={styles.textInput}
+                value={contact_phone}
+                onChangeText={text => set_contact_phone(text)}
               />
             </View>
           </View>
@@ -390,6 +506,8 @@ const DangTin = ({navigation}) => {
               <TextInput
                 placeholder="VD: tuanhaph09959@gmail.com"
                 style={styles.textInput}
+                value={contact_email}
+                onChangeText={text => set_contact_email(text)}
               />
             </View>
           </View>
@@ -399,15 +517,24 @@ const DangTin = ({navigation}) => {
               justifyContent: 'center',
               marginTop: scale(40),
             }}>
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                onSubmit();
+                navigation.navigate('TD_Screen');
+              }}>
               <Text style={styles.btnL}>Đăng tin</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('TD_Screen')}>
               <Text style={styles.btnR}>hủy</Text>
             </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
+      <ModalStyle
+        isVisible={modal}
+        onBackdropPress={() => setModal(false)}
+        content={error}
+      />
     </View>
   );
 };
