d2d35e4778cd1d2ca5594c6dda28b890cf003304
phongmc95 <phongdev95@gmail.com>
2021-12-07 22:42:02 +0700

    new code dang tin

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,14 +3,14 @@ import ForgotPassword from './ForgotPassword';
 import LogIn from './LogIn';
 import ProfileEmployer from './ProfileEmployer';
 import AddJob from './AddJob';
-import POSTUPDATEJOB from "./UpdateJob";
+import POSTUPDATEJOB from './UpdateJob';
 
 const rootReducer = combineReducers({
   Authen: LogIn,
   Forgot: ForgotPassword,
   ProfileEPl: ProfileEmployer,
   AddJob: AddJob,
-  UpDateJob:POSTUPDATEJOB,
+  UpDateJob: POSTUPDATEJOB,
 });
 
 export default (state, action) => rootReducer(state, action);
